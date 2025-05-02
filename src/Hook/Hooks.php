d7a3b077c6172dac125fd670c4440e8fc47a4451
Adam Bramley <adam.bramley@previousnext.com.au>
2025-05-02 14:57:39 +1000

    Standard linting (#7)

--- src/Hook/Hooks.php
+++ src/Hook/Hooks.php
@@ -11,16 +11,16 @@ use PreviousNext\Ds\Nsw\Utility\Twig;
 final class Hooks {
 
   #[Hook('system_info_alter')]
-  function systemInfoAlter(array &$info, Extension $file, string $type): void {
+  public function systemInfoAlter(array &$info, Extension $file, string $type): void {
     if ('pnx_ds_nsw' === $file->getName()) {
-      $dir = \Safe\realpath(sprintf('%s/../components/design-system/', \DRUPAL_ROOT));
+      $dir = \Safe\realpath(\sprintf('%s/../components/design-system/', \DRUPAL_ROOT));
 
       // In components/ComponentsRegistry, ltrim disallows absolute dirs, so we
       // must recompute where vendor is in relation to the DrupalRoot, even if
       // it means navigating below Drupal.
       // '/' indicates relative to DRUPAL_ROOT, not disk-root.
-      //https://www.drupal.org/project/components/issues/3210853
-      $info['components']['namespaces'][Twig::Namespace] = '/' . \PreviousNext\Ds\Common\Utility\Twig::computePathFromDrupalRootTo($dir);
+      // https://www.drupal.org/project/components/issues/3210853
+      $info['components']['namespaces'][Twig::NAMESPACE] = '/' . \PreviousNext\Ds\Common\Utility\Twig::computePathFromDrupalRootTo($dir);
 
       $info['components']['namespaces']['nswds'] = '/' . \PreviousNext\Ds\Common\Utility\Twig::computePathFromDrupalRootTo($dir);
     }
